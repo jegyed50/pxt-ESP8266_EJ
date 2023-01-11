38d511f99b7179e48130300df2b14f0fda777505
Egyed József <jegyed50@gmail.com>
2023-01-11 17:04:44 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -77,7 +77,7 @@ namespace pxtESP8266EJ {
     export function connectThingSpeak(ip: string, write_api_key: string, n1: number, n2: number, n3: number, n4: number, n5: number, n6: number, n7: number, n8: number) {
         if (wifi_connected && write_api_key != "") {
             thingspeak_connected = false
-            sendAT("AT+CIPSTART=\"TCP\",\"" + ip + "\",443", 0) // connect to website server
+            sendAT("AT+CIPSTART=\"TCP\",\"" + ip + "\",80", 0) // connect to website server
             thingspeak_connected = waitResponse()
             basic.pause(100)
             if (thingspeak_connected) {
@@ -90,7 +90,34 @@ namespace pxtESP8266EJ {
             }
         }
     }
-
+  /**
+    * Connect to ThingSpeak and upload data with SSL protocol. It would not upload anything if it failed to connect to Wifi or ThingSpeak.
+    */
+    //% block="Connect and Upload data (HTTPS) to ThingSpeak|URL/IP = %ip|Write API key = %write_api_key|Field 1 = %n1|Field 2 = %n2|Field 3 = %n3|Field 4 = %n4|Field 5 = %n5|Field 6 = %n6|Field 7 = %n7|Field 8 = %n8"
+    //% ip.defl=api.thingspeak.com
+    //% write_api_key.defl=your_write_api_key
+    export function connectThingSpeakHTTPS(ip: string, write_api_key: string, n1: number, n2: number, n3: number, n4: number, n5: number, n6: number, n7: number, n8: number) {
+        if (wifi_connected && write_api_key != "") {
+            thingspeak_connected = false
+            sendAT("AT+CIPSTART=\"TCP\",\"" + ip + "\",80", 0) // connect to website server
+            thingspeak_connected = waitResponse()
+            basic.pause(100)
+            if (thingspeak_connected) {
+                last_upload_successful = false
+               // HTTP 
+                //let str: string = "GET /update?api_key=" + write_api_key + "&field1=" + n1 + "&field2=" + n2 + "&field3=" + n3 + "&field4=" + n4 + "&field5=" + n5 + "&field6=" + n6 + "&field7=" + n7 + "&field8=" + n8
+               //HTTPS
+                let str: string = "AT+HTTPCLIENT=2,0,https://api.thingspeak.com/update?api_key=" + write_api_key + "&field1=" + n1 + "&field2=" + n2 + "&field3=" + n3 + "&field4=" + n4 + "&field5=" + n5 + "&field6=" + n6 + "&field7=" + n7 + "&field8=" + n8+",,,2"
+               // HTTP
+                //sendAT("AT+CIPSEND=" + (str.length + 2))
+               // HTTPS
+                // Minta AT+HTTPCLIENT=2,0,"https://www.espressif.com/sites/all/themes/espressif/images/about-us/solution-platform.jpg",,,2
+                sendAT(str, 0) // upload data
+                last_upload_successful = waitResponse()
+                basic.pause(100)
+            }
+        }
+    }
     /**
     * Wait between uploads
     */
